a00b5d17c6b826aa734c6b04cc34102ca8f34a85
JPietschmann <pj7349@mci4me.at>
2025-03-24 14:54:49 +0100

    Add files via upload
    Aufgabe 3

---
+++ main.py
@@ -0,0 +1,7 @@
+from my_functions import build_experiment
+
+build_experiment('test A', '23.3.25', 'Julia Pietschmann', 'Anton Antonson')
+#print(build_experiment('test A', '23.3.25', 'Julia Pietschmann', 'Anton Antonson'))
+
+if __name__ == "__main__":
+    print (build_experiment('test A', '23.3.25', 'Julia Pietschmann', 'Anton Antonson'))
