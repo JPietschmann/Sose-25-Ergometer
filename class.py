67c413559255502cc954d02de6edfd103405f0a1
JPietschmann <pj7349@mci4me.at>
2025-03-27 11:53:21 +0100

    Klasse Pferd erstellt

---
+++ class.py
@@ -0,0 +1,16 @@
+class Pferd ():
+    def __init__ (self, height, speed, name):
+        self.height= height
+        self.speed= speed
+        self.name= name
+
+    def vorstellen(self):
+        print("Ich heiße", name, ", bin", height, "groß und", speed, "schnell.")
+    
+pferd1= Pferd("1,73m", "35km/h", "Big Bitch")
+pferd2= Pferd("1,84m", "48km/h", "Fast As Fuack Boi")
+pferd3= Pferd("1,79m", "39km/h", "Massive Cunt")
+
+print(vorstellen.pferd1)
+print(vorstellen.pferd2)
+print(vorstellen.pferd3)
